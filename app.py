0d64fb0dff82d01f8e272b84e929cf39663824e5
saparj <justin.sapar@gmail.com>
2026-03-06 23:48:05 -0500

    fix: dbpath fixed and no longer breaks if overriding environmental variable

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ load_dotenv()
 
 app = Flask(__name__)
 
-DB_PATH = Path("DB_PATH", "/var/lib/phone-services/requests.db")
+DB_PATH = Path(os.getenv("DB_PATH", "/var/lib/phone-services/requests.db"))
 BASE_URL = os.getenv("BASE_URL", "http://example.local")
 ADMIN_USERS = set(
     u.strip()
